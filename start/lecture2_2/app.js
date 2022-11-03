081bd247a47b36288f05de1e3c9aa4282306eb22
skJaganFalconicx <117151077+skJaganFalconicx@users.noreply.github.com>
2022-11-03 17:38:01 +0530

    copied example

--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -6,20 +6,20 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
 
-        this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
-        this.camera.position.set( 0, 0, 4 );
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
+        this.camera.position.set(0, 0, 4);
 
         this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0xaaaaaa );
-        
-		this.renderer = new THREE.WebGLRenderer({ antialias: true} );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-		container.appendChild( this.renderer.domElement );
+        this.scene.background = new THREE.Color(0xaaaaaa);
+
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        container.appendChild(this.renderer.domElement);
 
         this.renderer.setAnimationLoop(this.render.bind(this));
-        
-        window.addEventListener('resize', this.resize.bind(this) );
+
+        window.addEventListener('resize', this.resize.bind(this));
 	}	
     
     resize(){
@@ -27,7 +27,7 @@ class App{
     }
     
     render() {
-        this.renderer.render( this.scene, this.camera );
+        this.renderer.render(this.scene, this.camera);
     }
 }
 
