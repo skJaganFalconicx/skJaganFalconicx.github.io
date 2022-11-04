56d6c3b91ab82ca66b4bbdf9f84e0712b62a25e4
skJaganFalconicx <117151077+skJaganFalconicx@users.noreply.github.com>
2022-11-04 13:06:45 +0530

    test

--- start/lecture2_6/app.js
+++ start/lecture2_6/app.js
@@ -76,7 +76,7 @@ class App{
             function (xhr) {
                 self.loadingBar.progress = xhr.loaded / xhr.total;
             },
-            function (error) {
+            function (err) {
                 console.log('Error');
             }
         )
